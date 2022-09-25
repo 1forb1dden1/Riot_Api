7e8b182fdee188b897f61fe9ef64c4349f436203
Kevin Chau <90816621+1forb1dden1@users.noreply.github.com>
2022-09-25 04:57:32 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,51 +2,133 @@ import React, { useState } from "react";
 import axios from 'axios';
 import {servers, ranks} from "./data"
 import './App.css';
+ 
+const RANKED_SOLO_5v5 = "RANKED_SOLO_5x5";
+const RANKED_FLEX_SR = "RANKED_FLEX_SR";
+const RANKED_TFT = "RANKED_FLEX_TFT";
 
-// create playerData interface
-// create PlayerStats interface. 
+interface Summoner {
+  id: string;
+  accountId: string;
+  puuid: string;
+  name: string;
+  profileIconId: number;
+  revisionDate: number;
+  summonerLevel: number;
+}
+
+interface Player_Ranked_Solo_Stats {
+  summonerName: string;
+  tier: string;
+  rank: string;
+  leaguePoints: number;
+  wins: number;
+  losses: number;
+  winRate: number;
+}
+
+interface Player_Ranked_Flex_Stats {
+  summonerName: string;
+  tier: string;
+  rank: string;
+  leaguePoints: number;
+  wins: number;
+  losses: number;
+  winRate: number;
+}
+
+interface Player_Ranked_TFT_Stats{
+  summonerName: string;
+  tier: string;
+  rank: string;
+  leaguePoints: number
+}
 
 function App() {
-  const API_KEY ="RGAPI-3fe92e43-bf2b-4082-95e7-c5c9fe5e2ab6";
-  const patch = "12.17.1";
-  const [playerData, setPlayerData] = useState<any>({});
+  const API_KEY ="";
+  const patch = "12.18";
+  const [playerProfile, setPlayerProfile] = useState<any>({});
   const [playerStats, setPlayerStats] = useState<any>({});
   const [searchText, setSearchText] = useState<string>("");
   const [serverName, setServerName] = useState<string>("na1");
+  const [value, setValue] = useState<any>(0);
+
+  function getMatchData() {
+
+  }
 
   function searchForPlayer(input : any) {
     var ApiCallString = "https://" + serverName + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + searchText + "?api_key=" + API_KEY;
     axios.get(ApiCallString).then(function(response){
-      setPlayerData(response.data);
-      //console.log(response.data);
+      setPlayerProfile(response.data);
       var ApiCallString2 = "https://" + serverName + ".api.riotgames.com/lol/league/v4/entries/by-summoner/" + response.data.id + "?api_key=" + API_KEY;
       axios.get(ApiCallString2).then(function(response){
         setPlayerStats(response.data);
-        //console.log(response.data);
       });
     }).catch (function (error){
       console.log(error);
-      setPlayerData({});
+      setPlayerProfile({});
     });
   }
 
-  const RankIndex = (playerStats: any, rankType: string) : any => {
-    //"RANKED_FLEX_SR"
-    //"RANKED_SOLO_5x5"
-    //"RANKED_TFT_DOUBLE_UP"
-    //"RANKED_TFT"
-    for( var i = 0 ; i < playerStats.length ; i++){
+  const getRankIndex = (playerStats: any, rankType: string) : any => {
+    for ( var i = 0 ; i < playerStats.length ; i++){
       if(playerStats[i].queueType == rankType){
         return i;
       }
     }
   }
+
+  function renderPlayerProfile(){
+    if(JSON.stringify(playerProfile) != '{}'  )
+    {
+      return (
+        <div>
+          <p>{playerProfile.name}</p>
+          <img width="150" height="150" src={"https://ddragon.leagueoflegends.com/cdn/" + patch + "/img/profileicon/" + playerProfile.profileIconId + ".png"}></img>
+          <p>Summoner Level {playerProfile.summonerLevel}</p>
+          <p>Solo Queue </p>
+        </div>
+      );
+    }
+    else
+    {
+      return (
+        <div>
+          <p>Player Not Found</p>
+        </div>
+      )
+    }
+  }
+  function renderPlayerStats(gameMode: string){
+    if(playerStats[getRankIndex(playerStats, gameMode)] != null)
+    {
+      var winrate = 100*playerStats[getRankIndex(playerStats, gameMode)].wins / (playerStats[getRankIndex(playerStats, gameMode)].wins + playerStats[getRankIndex(playerStats, gameMode)].losses);
+      return (
+        <div>
+          <p>{playerStats[getRankIndex(playerStats, gameMode)].tier} {playerStats[getRankIndex(playerStats, gameMode)].rank}</p>
+          <img width="150" height="150" src={"../src/img/lolRanks/Emblem_" + playerStats[getRankIndex(playerStats, gameMode)].tier + ".png"} alt ="rank"/>
+          <p>{playerStats[getRankIndex(playerStats, gameMode)].leaguePoints} LP</p>
+          <p>{playerStats[getRankIndex(playerStats, gameMode)].wins}W {playerStats[getRankIndex(playerStats, gameMode)].losses}L</p>
+          <p>{Math.round(winrate)}% Win Rate</p>
+        </div>
+      );
+    }
+    else{
+      return (
+        <div>
+          <p>5v5 Flex Queue</p>
+          <p>Unranked</p>
+        </div>
+      )
+    }
+  }
   
   return (
     <div className="App">
-      <div className="container">
+      <div className="NavBar">
+        <div className="container">
         <h3>League of Legends Player Searcher</h3>
-
         <select value={serverName} onChange={e => setServerName(e.target.value)}>
           {servers.map((s) => (
             <option key={s.value} value={s.value}>
@@ -54,64 +136,12 @@ function App() {
             </option>
           ))}
         </select>
-
         <input type="text" onChange={e => setSearchText(e.target.value)}></input>
         <button onClick={e => searchForPlayer(e)}>Search for Player</button>
-      </div>
-      <div className = "RANKED_SOLO_5x5">
-        {JSON.stringify(playerData[RankIndex(playerStats, "RANKED_SOLO_5x5")]) != '{}' && JSON.stringify(playerStats) != '{}'?
-          <>
-            <p>{playerData.name}</p>
-            <img width="150" height="150" src={"https://ddragon.leagueoflegends.com/cdn/" + patch + "/img/profileicon/" + playerData.profileIconId + ".png"}></img>
-            <p>Summoner Level {playerData.summonerLevel}</p>
-            <p>Solo Queue </p>
-              {
-                playerStats[RankIndex(playerStats, "RANKED_SOLO_5x5")] != undefined ? 
-                <> 
-                <p>
-                  {playerStats[RankIndex(playerStats, "RANKED_SOLO_5x5")].tier} {playerStats[RankIndex(playerStats, "RANKED_SOLO_5x5")].rank} {playerStats[RankIndex(playerStats, "RANKED_SOLO_5x5")].leaguePoints} lp
-                </p>
-                <p>
-                  {playerStats[RankIndex(playerStats, "RANKED_SOLO_5x5")].wins}W {playerStats[RankIndex(playerStats, "RANKED_SOLO_5x5")].losses}L
-                </p>
-                <img width="150" height="150" src={"../src/img/lolRanks/Emblem_" + playerStats[RankIndex(playerStats, "RANKED_SOLO_5x5")].tier + ".png"} alt ="rank"/>
-                </>
-                : 
-                <> 
-                  <p>Unranked</p> 
-                </>
-              }
-          </> :
-          <>
-            <p>No player data</p> 
-          </>
-        }
-      </div>
-      <div className = "RANKED_FLEX_SR">
-        {JSON.stringify(playerData) != '{}' && JSON.stringify(playerStats) != '{}'?
-          <>
-            <p>Flex Queue</p>
-              {
-                playerStats[RankIndex(playerStats, "RANKED_FLEX_SR")] != undefined ?
-                <>      
-                <p>
-                  {playerStats[RankIndex(playerStats, "RANKED_FLEX_SR")].tier} {playerStats[RankIndex(playerStats, "RANKED_FLEX_SR")].rank} {playerStats[RankIndex(playerStats, "RANKED_FLEX_SR")].leaguePoints} lp
-                </p>
-                <p>
-                  {playerStats[RankIndex(playerStats, "RANKED_FLEX_SR")].wins}W {playerStats[RankIndex(playerStats, "RANKED_FLEX_SR")].losses}L
-                </p>
-                <img width="150" height="150" src={"../src/img/lolRanks/Emblem_" + playerStats[RankIndex(playerStats, "RANKED_FLEX_SR")].tier + ".png"} alt ="rank"/>
-                </>
-                :
-                <>
-                  <p>Unranked</p>
-                </> 
-              }
-          </> :
-          <>
-            <p>No player data</p>
-          </>
-        }
+        </div>
+        {renderPlayerProfile()}
+        {renderPlayerStats(RANKED_SOLO_5v5)}
+        {renderPlayerStats(RANKED_FLEX_SR)}
       </div>
     </div>
   );
